bfea332fd859dc8fd5158c97bbad1eabdbc2bb03
elenaisabella02 <bragaelenaisabella@yahoo.com>
2023-04-06 13:34:18 +0300

    Added LastName for exercise 4

---
+++ Tap.School/Tap.School.Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tap.School.Api.Entities;
+
+namespace Tap.School.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Tap.School/Tap.School.Api/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Tap.School.Api.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        //added LastName for exercise 4
+        public string LastName { get; set; }
+    }
+}
